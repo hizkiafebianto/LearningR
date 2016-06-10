734ee38ef0721bd69018bafd16511a01bef800a9
hizkiafebianto <hizkia.febianto@gmail.com>
2016-06-09 20:04:24 -0400

    Initial Commit

---
+++ test.R
@@ -0,0 +1,14 @@
+stateData <- read.csv("D:/MBA Journey/2016 - Summer Project/Learning R/Udacity Course/lesson2/stateData.csv", stringsAsFactors = F)
+summary(stateData)
+str(stateData)
+names(stateData)
+
+library(ggplot2)
+reddit <- read.csv("D:/MBA Journey/2016 - Summer Project/Learning R/Udacity Course/lesson2/reddit.csv")
+str(reddit)
+qplot(data = reddit, x = age.range)
+reddit$age.range <- factor(reddit$age.range, levels = c("Under 18","18-24","25-34","35-44","45-54","55-64","65 or above","NA"), ordered = T)
+head(reddit$age.range, 10)
+
+
+
